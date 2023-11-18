a79c332cd88fab0ee4dd104985dde6ff4b50d99b
Kevin <kevin821224@gmail.com>
2023-11-18 16:01:24 +0800

    Update assignment1.c

--- Assignments/assignment1.c
+++ Assignments/assignment1.c
@@ -1,10 +1,10 @@
 #include <8052.h>
 
 void main() {
-while (1) {
-    P1 = 0x00;
-    for (int i = 0; i < 50000; i++);
-    P1 = 0xFF;
-    for (int i = 0; i < 50000; i++);
-}
+    while (1) {
+        P1 = 0x00;
+        for (int i = 0; i < 50000; i++);
+        P1 = 0xFF;
+        for (int i = 0; i < 50000; i++);
+    }
 }
